3c2d1d3ef325444026a052e44b6f9cea7f1bca69
Fabio <fabio.gondolo@gmail.com>
2018-03-11 23:21:43 +0100

    Create the basic Header
    Add the js code needed

---
+++ includes/partials/header.php
@@ -0,0 +1,23 @@
+<header class="navbar clearfix">
+    <a href="index.html" class="navbar__logo">Logo</a>
+    <div class="navbar__icon-bar js-toggle" data-target="navbar-menu">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+  
+    <nav>
+      <ul class="navbar__menu animate" id="navbar-menu">
+        <li class="navbar__menu__item"><a href="index.html">Home</a>
+        </li>
+        <li class="navbar__menu__item"><a href="single.html">Single</a>
+        </li>
+        <li class="navbar__menu__item"><a href="page.html">Page</a>
+        </li>
+        <li class="navbar__menu__item"><a href="">Item</a>
+        </li>
+        <li class="navbar__menu__item"><a href="">Item</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <!doctype html>
 <html>
+
 <head>
-<meta charset="utf-8">
-    <link rel="stylesheet" href="assets/css/main.css">
-<title>Documento senza titolo</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/main.css?t=<?php echo time(); ?>">
+  <title>Documento senza titolo</title>
 </head>
 
 <body>
-    <div class="button">CIaone</div>
+  <?php include "includes/partials/header.php"; ?>
+  <?php include "includes/partials/footer.php"; ?>
+  <script src="assets/js/base.js"></script>
+
 </body>
+
 </html>
