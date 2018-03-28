ee514a317b7fb4b3e88ab2742820f5f611d3d763
marco <gundul1@libero.it>
2018-03-28 18:32:17 +0200

    Create card and start create form

--- includes/partials/header.php
+++ includes/partials/header.php
@@ -1,29 +1,29 @@
 <header class="navbar clearfix">
-    <a href="index.html" class="navbar__logo">Logo</a>
-    <div class="navbar__icon-bar js-toggle" data-target="navbar-menu">
-			<span></span>
-			<span></span>
-			<span></span>
-		</div>
-  
-    <nav>
-      <ul class="navbar__menu animate" id="navbar-menu">
-        <li class="navbar__menu__item"><a href="index.html">Home</a>
-        </li>
-        <li class="navbar__menu__item"><a href="single.html">Single</a>
-        </li>
-        <li class="navbar__menu__item"><a href="page.html">Page</a>
-        </li>
-        <li class="navbar__menu__item dropdown js-dropdown">
-          <a href="" data-toggle="dropdown" class="pino" >Item</a>
-          <ul class="dropdown-menu js-dropdown-menu">
-            <li><a href="">First index</a></li>
-            <li><a href="">Second index</a></li>
-            <li><a href="">Third index</a></li>
-          </ul>
-        </li>
-        <li class="navbar__menu__item"><a href="">Item</a>
-        </li>
-      </ul>
-    </nav>
-  </header>
+<a href="index.html" class="navbar__logo">Logo</a>
+<div class="navbar__icon-bar js-toggle" data-target="navbar-menu">
+		<span></span>
+		<span></span>
+		<span></span>
+	</div>
+
+<nav>
+  <ul class="navbar__menu animate" id="navbar-menu">
+	<li class="navbar__menu__item"><a href="index.html">Home</a>
+	</li>
+	<li class="navbar__menu__item"><a href="single.html">Single</a>
+	</li>
+	<li class="navbar__menu__item"><a href="page.html">Page</a>
+	</li>
+	<li class="navbar__menu__item dropdown js-dropdown">
+	  <a href="" data-toggle="dropdown" class="pino" >Item</a>
+	  <ul class="dropdown-menu js-dropdown-menu">
+		<li><a href="">First index</a></li>
+		<li><a href="">Second index</a></li>
+		<li><a href="">Third index</a></li>
+	  </ul>
+	</li>
+	<li class="navbar__menu__item"><a href="">Item</a>
+	</li>
+  </ul>
+</nav>
+</header>
